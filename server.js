2dc78db1090b8211d615d8ba435dee1b7436d011
Lennard Dorst <lennardsolana@gmail.com>
2022-12-12 12:08:08 +0100

    Compiling

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 const express = require('express')
 const generate = require("./routers/generate")
+var cors = require('cors')
 
 const app = express()
+app.use(cors())
 
 app.use('/generate', generate)
 
